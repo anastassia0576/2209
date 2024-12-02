ba358d63fac372cf0dd3b4cbb207c2e37539368b
anastassia0576 <141334881+anastassia0576@users.noreply.github.com>
2024-12-02 15:56:20 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,156 +1,51 @@
 import streamlit as st
-import requests
-
 import folium
 from folium.plugins import Search, MarkerCluster
+from io import BytesIO
 
+# Данные GeoJSON
 geojson_data = {
     "type": "FeatureCollection",
     "features": [
         {
             "type": "Feature",
-      "properties": {"name": "Корпус агрономического факультета КазАТУ", "category": "Category B"},
-      "geometry": {
-        "coordinates": [
-          71.41037773710028,
-          51.186010264645375], "type": "Point"},
+            "properties": {"name": "Корпус агрономического факультета КазАТУ", "category": "Category B"},
+            "geometry": {"coordinates": [71.41037773710028, 51.186010264645375], "type": "Point"},
         },
         {
             "type": "Feature",
-      "properties": {"name": "Казахский агротехнический университет имени С. Сейфуллина, главный корпус"},
-      "geometry": {
-        "coordinates": [
-          71.40919952047997,
-          51.18714511950674], "type": "Point"},
+            "properties": {"name": "Казахский агротехнический университет имени С. Сейфуллина, главный корпус"},
+            "geometry": {"coordinates": [71.40919952047997, 51.18714511950674], "type": "Point"},
         },
-        {
-            "type": "Feature",
-      "properties": {"name": "Биологический корпус КазАТУ"},
-      "geometry": {
-        "coordinates": [
-          71.4094857538959,
-          51.18797538164955], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Корпус технического факультета КазАТУ"},
-      "geometry": {
-        "coordinates": [
-          71.41114324507092,
-          51.1877459137084], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Новый корпус технического факультета КазАТУ"},
-      "geometry": {
-        "coordinates": [
-          71.41172902508583,
-          51.18715346392423], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Корпус военной кафедры КазАТУ "},
-      "geometry": {
-        "coordinates": [
-          71.41081041551712,
-          51.18707001968042], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Склад КазАТУ "},
-      "geometry": {
-        "coordinates": [
-          71.41170239872159,
-          51.18659438460773], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Общежитие №7 КазАТУ "},
-      "geometry": {
-        "coordinates": [
-          71.41260769510728,
-          51.1863148424068], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Общежитие №2б КазАТУ "},
-      "geometry": {
-        "coordinates": [
-         71.41294052466108,
-          51.18694068080447], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Общежитие №9 КазАТУ "},
-      "geometry": {
-        "coordinates": [
-         71.4132400712582,
-          51.18633153154096], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Общежитие №4 КазАТУ "},
-      "geometry": {
-        "coordinates": [
-         71.41429301517488,
-          51.18643214747047], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Корпус земельного факультета КазАту "},
-      "geometry": {
-        "coordinates": [
-         71.41529891243619,
-          51.18658181999851], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Общежитие №5 КазАТУ "},
-      "geometry": {
-        "coordinates": [
-         71.41385674718038,
-          51.18641280852361], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Гараж КазАТУ "},
-      "geometry": {
-        "coordinates": [
-         71.41107883675795,
-          51.186700066345196], "type": "Point"},
-        },
-        {
-            "type": "Feature",
-      "properties": {"name": "Корпус кафедры технической механики КазАТУ "},
-      "geometry": {
-        "coordinates": [
-         71.41207484233962,
-          51.186695146518645], "type": "Point"},
-        },
-    ],
-
+        # Добавьте другие объекты в том же формате
+    ]
 }
 
+# Создание карты с начальной позицией
 m = folium.Map(location=[51.186845, 71.41058], zoom_start=14)
 
+# Группировка маркеров
 marker_cluster = MarkerCluster()
 
 for feature in geojson_data["features"]:
     coords = feature["geometry"]["coordinates"]
     name = feature["properties"]["name"]
-
-    popup_content = f"<b>Name:</b> {name}<br><b>Category:</b>"
-
+    
+    # Создаем всплывающее окно с информацией
+    popup_content = f"<b>Name:</b> {name}<br><b>Category:</b> {feature['properties']['category']}"
+    
     marker = folium.Marker(
-        location=[coords[1], coords[0]],
+        location=[coords[1], coords[0]], 
         popup=popup_content,
         tooltip=name,
         icon=folium.Icon(color="blue", icon="info-sign"),
     )
     marker_cluster.add_child(marker)
 
+# Добавляем кластер маркеров на карту
 m.add_child(marker_cluster)
 
+# Добавляем слой GeoJSON с поиском
 geojson_layer = folium.GeoJson(
     geojson_data,
     name="GeoJSON Data",
@@ -159,6 +54,7 @@ geojson_layer = folium.GeoJson(
 )
 m.add_child(geojson_layer)
 
+# Добавляем компонент поиска на карте
 search = Search(
     layer=geojson_layer,
     geom_type="Point",
@@ -167,10 +63,13 @@ search = Search(
     collapsed=False,
 ).add_to(m)
 
-
-
+# Добавляем управление слоями
 folium.LayerControl().add_to(m)
 
-m.save("interactive_map.html")
+# Сохранение карты в объект BytesIO
+map_html = BytesIO()
+m.save(map_html)
+map_html.seek(0)
 
-m
+# Встраивание карты в Streamlit
+st.components.v1.html(map_html.getvalue().decode(), height=600)
